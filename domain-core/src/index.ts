343d46bfcf4d4c11572495f74c84d89e8b93c3a4
emamaz <santiagomaza33@gmail.com>
2025-10-20 14:49:11 -0300

    Export of the index file from the errors folder

--- domain-core/src/index.ts
+++ domain-core/src/index.ts
@@ -1,4 +1,5 @@
 export * from "./repository/index";
 export * from "./entities/index";
 export * from "./use-cases/index";
-export * from "./enums/index"
+export * from "./enums/index";
+export * from "./errors/index";
