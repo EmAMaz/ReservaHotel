b02e7794c20dba14044a4e1940c1a933d76929b1
emamaz <santiagomaza33@gmail.com>
2025-10-20 14:42:45 -0300

    improved error checking by applying classes

--- domain-core/src/use-cases/guestUsesCases.ts
+++ domain-core/src/use-cases/guestUsesCases.ts
@@ -1,4 +1,5 @@
 import { Guest } from "../entities/Guest";
+import { ConflictError, NotFoundError } from "../errors";
 import { GuestRepository } from "../repository";
 
 export class GuestUsesCases implements GuestRepository {
@@ -8,18 +9,33 @@ export class GuestUsesCases implements GuestRepository {
     const result = await this.guestRepository.getAll();
     return result;
   }
-  async getById(id: string): Promise<Guest> {
+  async getById(id: string): Promise<Guest | null> {
     const result = await this.guestRepository.getById(id);
+    if(!result) throw new NotFoundError("Guest not found");
     return result;
   }
-  async save(guest: Omit<Guest, "id">): Promise<Guest> {
-    const result = await this.guestRepository.save(guest);
+
+  async findByEmail(email: string): Promise<Guest | null> {
+    const result = await this.guestRepository.findByEmail(email);
     return result;
   }
-  async update(id: string, guest: Omit<Guest, "id">): Promise<void> {
+
+  async save(guest: Omit<Guest, "id">): Promise<void> {
+    const findByEmail = await this.findByEmail(guest.email);
+    if (findByEmail) throw new ConflictError();
+
+    await this.guestRepository.save(guest);
+  }
+  async update(id: string, guest: Omit<Guest, "id" | "email">): Promise<void> {
+    const findById = await this.guestRepository.getById(id);
+    if (!findById) throw new NotFoundError();
+
     await this.guestRepository.update(id, guest);
   }
   async delete(id: string): Promise<void> {
-    await this.guestRepository.delete(id);
+    const findById = await this.guestRepository.getById(id);
+    if (!findById) throw new NotFoundError();
+    
+    await this.guestRepository.delete(Number(id));
   }
 }
