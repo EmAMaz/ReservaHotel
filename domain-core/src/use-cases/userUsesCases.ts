4b3411c57bfb11b0c8c7ce2b7bf6e2e4465f5587
emamaz <santiagomaza33@gmail.com>
2025-10-20 14:47:28 -0300

    Change in the return value of the save method

--- domain-core/src/use-cases/userUsesCases.ts
+++ domain-core/src/use-cases/userUsesCases.ts
@@ -2,11 +2,11 @@ import { User } from "../entities/User";
 import { UserRepository } from "../repository/UserRepository";
 
 export class UserUsesCases implements UserRepository {
+export class UserUsesCases {
   constructor(private userRepository: UserRepository) {}
 
-  async save(user: Omit<User, "id">): Promise<User> {
-    const result = await this.userRepository.save(user);
-    return result;
+  async save(user: Omit<User, "id">): Promise<void> {
+    await this.userRepository.save(user);
   }
 
   async login(email: string, password: string): Promise<User> {
