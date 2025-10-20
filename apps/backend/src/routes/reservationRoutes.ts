1f9bbb98c1170ece7ec48df4c2a001b7b539d94d
emamaz <santiagomaza33@gmail.com>
2025-10-20 14:49:33 -0300

    Implementation of new routes

--- apps/backend/src/routes/reservationRoutes.ts
+++ apps/backend/src/routes/reservationRoutes.ts
@@ -7,5 +7,7 @@ export function createReservationRouter(controller: ReservationController): Rout
   router.post("/", controller.create.bind(controller));
   router.get("/:id", controller.getById.bind(controller));
   router.get("/", controller.getAll.bind(controller));
+  router.put("/:id", controller.update.bind(controller))
+  router.delete("/:id", controller.delete.bind(controller));
   return router;
 }
