2b7a35f565f0a9c4af200d8e33e51247f02bc382
emamaz <santiagomaza33@gmail.com>
2025-10-20 18:51:12 -0300

    Initial frontend created

---
+++ apps/frontend/src/App.tsx
@@ -0,0 +1,12 @@
+
+function App() {
+  return (
+    <>
+      <div className="flex justify-center items-center h-screen">
+        <h1 className='text-2xl font-bold'>Hola mundo</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
