851529068beccfe18b04fdddf2ef58dc00bba012
emamaz <santiagomaza33@gmail.com>
2025-12-31 14:49:54 -0300

    Configuration settings

--- apps/frontend/src/main.tsx
+++ apps/frontend/src/main.tsx
@@ -1,10 +1,19 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.tsx";
+import { Toaster } from "react-hot-toast";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
-createRoot(document.getElementById('root')!).render(
+const queryClient = new QueryClient();
+
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <QueryClientProvider client={queryClient}>
+      <App />
+      <ReactQueryDevtools initialIsOpen={false} />
+      <Toaster position="top-right" reverseOrder={false} gutter={8}></Toaster>
+    </QueryClientProvider>
+  </StrictMode>
+);
