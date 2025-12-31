e5d5683f5e09d572b28876deced87b523e795607
emamaz <santiagomaza33@gmail.com>
2025-12-31 15:01:16 -0300

    Implementation of protected routes and authProvider

--- apps/frontend/src/App.tsx
+++ apps/frontend/src/App.tsx
@@ -1,12 +1,53 @@
+import { BrowserRouter, Route, Routes } from "react-router";
+import { AuthProvider } from "./context/AuthContext";
+import { Dashboard } from "./components/Dashboard";
+import { Reservation } from "./components/Reservation";
+import { PageMain } from "./components/PageMain";
+import { Login } from "./components/auth/Login";
+import { Register } from "./components/auth/Register";
+import { ProtectedRoute } from "./components/auth/ProtectedRoute";
+import { Admin } from "./components/Admin";
+import { ReservationMade } from "./components/ReservationMade";
+import { RoomForm } from "./components/editPage";
 
 function App() {
   return (
     <>
-      <div className="flex justify-center items-center h-screen">
-        <h1 className='text-2xl font-bold'>Hola mundo</h1>
-      </div>
+      <section className="flex justify-center min-w-screen min-h-screen">
+      <AuthProvider>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<PageMain />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+            <Route element={<ProtectedRoute />}>
+              <Route path="/dashboard">
+                <Route index element={<Dashboard />} />
+                <Route path="reservations-made" element={<ReservationMade />} />
+                <Route
+                  path="reservations/:categoryId"
+                  element={<Reservation />}
+                />
+              </Route>
+            </Route>
+            <Route element={<ProtectedRoute requiredRole="ADMIN" />}>
+              <Route path="/admin" element={<Admin />} />
+              <Route
+                path="/admin/users"
+                element={<h2>Gestión de Usuarios</h2>}
+              />
+              <Route
+                path="/admin/edit-room/:roomId"
+                element={<RoomForm />}
+              />
+            </Route>
+            <Route path="*" element={<h2>404 Not Found</h2>} />
+          </Routes>
+        </BrowserRouter>
+      </AuthProvider>
+    </section>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
