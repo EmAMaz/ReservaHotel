3483790b4688004e2a8a2e5a85ff6c220ebdd6fe
emamaz <santiagomaza33@gmail.com>
2025-12-31 14:04:44 -0300

    repository adjustments

--- domain-core/src/repository/ReservationRepository.ts
+++ domain-core/src/repository/ReservationRepository.ts
@@ -3,6 +3,7 @@ import { Reservation } from "../entities/Reservation";
 export interface ReservationRepository {
     getAll(): Promise<Reservation[]>;
     getById(id: string): Promise<Reservation | null>;
+    getByUserId(userId: string): Promise<Reservation[]>;
     save(reservation: Omit<Reservation, "id">): Promise<void>;
     update(id: string, reservation: Omit<Reservation, "id">): Promise<void>;
     delete(id: string): Promise<void>;
